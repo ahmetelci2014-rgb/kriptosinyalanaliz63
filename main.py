b27902b82a6d5d907c5cc1fd7648f266542c86e0
ahmetelci2014-rgb <ahmetelci2014@gmail.com>
2026-07-02 10:24:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ TOKEN = "8619346423:AAFKQN6x6c1IreXc007VXvSB0gtemEwqhXg"
 CHAT_ID = "8439391876"
 
 try:
-    r = requests.get(
-        "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
-        timeout=10
-    )
+    btc = requests.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    ).json()
 
-    veri = r.json()
+    eth = requests.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+    ).json()
 
-    mesaj = f"Binance cevabı:\n{veri}"
+    mesaj = f"""
+🚀 KRİPTO SİNYAL BOTU
+
+BTC: ${btc['bitcoin']['usd']}
+ETH: ${eth['ethereum']['usd']}
+
+✅ CoinGecko bağlantısı başarılı.
+"""
 
 except Exception as e:
-    mesaj = f"Hata:\n{str(e)}"
+    mesaj = f"❌ Hata:\n{e}"
 
 requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
