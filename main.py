8cc6b6192a71c694abe30443172132882eda7723
ahmetelci2014-rgb <ahmetelci2014@gmail.com>
2026-07-02 10:02:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-import os
 import requests
 
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = "8619346423:AAGAXRkFwUD7Qy3l0MoggiKpOJzKOFtDZUY"
+CHAT_ID = "8439391876"
 
-try:
-    response = requests.get(
-        "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-    )
+mesaj = "✅ TEST MESAJI\nGitHub Actions başarılı çalıştı."
 
-    data = response.json()
-
-    if "price" in data:
-        mesaj = f"""🚀 KRİPTO SİNYAL BOTU
-
-Coin: BTCUSDT
-Fiyat: {data['price']}
-
-✅ Bot başarıyla çalışıyor.
-"""
-    else:
-        mesaj = f"❌ Binance API Hatası:\n{data}"
-
-except Exception as e:
-    mesaj = f"❌ Hata oluştu:\n{e}"
-
-requests.post(
+r = requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
         "text": mesaj
     }
 )
+
+print(r.text)
